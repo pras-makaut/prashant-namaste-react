8f54fcc19b49374bb9fbc8520c6cc882767784f4
Ranjan_ Prashant (prasranjan@deloitte.com) <prasranjan@your.hostname.com>
2023-04-11 08:52:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>This is h1 tag</h1>
+ *      </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is h1 tag"),
+    React.createElement("h2", {}, "This is h2 tag"),
+  ])
+);
+
+const heading = React.createElement(
+  "h1",
+  { className: "heading" },
+  "Hello World from React!"
+);
+console.log(heading); //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(parent);
