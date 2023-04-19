b81a9a0ab5af5ac0843a73d79568f026b95a1e8b
Ranjan_ Prashant (prasranjan@deloitte.com) <prasranjan@your.hostname.com>
2023-04-19 08:33:13 +0530

    explore the world

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,89 @@
 import RestaurentCard from "./RestaurentCard";
 import resObj from "../utils/mockdata";
+import Shimmer from "./Shimmer";
+import { useState, useEffect } from "react";
+
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name?.toLowerCase()?.includes(searchText?.toLowerCase())
+  );
+  return filterData;
+}
+
 const Body = () => {
-  return (
+  const [listOfRestaurent, setListOfRestaurent] = useState([]);
+  const [filterRestaurent, setFilterRestaurent] = useState([]);
+  //normal java script variabel
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    getRestaurents();
+  }, []);
+  async function getRestaurents() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940499&lng=85.1376051&page_type=DESKTOP_WEB_LISTING#"
+    );
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurent(json?.data?.cards[2]?.data?.data?.cards);
+    setFilterRestaurent(json?.data?.cards[2]?.data?.data?.cards);
+  }
+  //Early return or not render component
+
+  if (listOfRestaurent?.length === 0) {
+    return <Shimmer />;
+  }
+
+  return filterRestaurent?.length === 0 ? (
+    <>
+      <div className="filter">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterListOfRestaurent = filterData(
+              searchText,
+              listOfRestaurent
+            );
+            setFilterRestaurent(filterListOfRestaurent);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <h1>No filtered Restaurents</h1>
+    </>
+  ) : (
     <div className="body">
-      <div className="serach">Search</div>
+      <div className="filter">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterListOfRestaurent = filterData(
+              searchText,
+              listOfRestaurent
+            );
+            setFilterRestaurent(filterListOfRestaurent);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <div className="res-container">
-        {resObj.map((ele, idx) => (
+        {filterRestaurent.map((ele, idx) => (
           <RestaurentCard key={ele.data.id} resData={ele} />
         ))}
         {/* <RestaurentCard resName="KFC" cusins="Burger Fried Chicken" /> */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 
 const Header = () => {
+  const [logIn, setLogIn] = useState(true);
   return (
     <div className="header">
       <div className="logoContainer">
@@ -14,6 +16,11 @@ const Header = () => {
           <li>Cart</li>
         </ul>
       </div>
+      {logIn === true ? (
+        <button onClick={() => setLogIn(false)}>Login </button>
+      ) : (
+        <button onClick={() => setLogIn(true)}>LogOut </button>
+      )}
     </div>
   );
 };
